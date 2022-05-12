8a556e2f52005e5a1f3c835c316d310204837ca9
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-12 23:13:11 +0300

    cors added

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(function (req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
   )
+  res.header('Access-Control-Allow-Credentials', 'true')
   next()
 })
 
