486107966cb3147e8bb10e3ccea10e4bdfd7c6dc
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-12 21:55:51 +0300

    initial commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,48 +1,63 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require('../model/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const handleLogin = async (req, res) => {
-    const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+  const { user, pwd } = req.body
 
-    const foundUser = await User.findOne({ username: user }).exec();
-    if (!foundUser) return res.sendStatus(401); //Unauthorized 
-    // evaluate password 
-    const match = await bcrypt.compare(pwd, foundUser.password);
+  if (!user || !pwd) {
+    return res
+      .status(400)
+      .json({ message: 'Username and password are required.' })
+  }
+
+  const foundUser = await User.findOne({ username: user }).exec()
+
+  if (!foundUser) {
+    return res.sendStatus(401) //Unauthorized
+  } else {
+    // evaluate password
+    const match = await bcrypt.compare(pwd, foundUser.password)
+
     if (match) {
-        const roles = Object.values(foundUser.roles).filter(Boolean);
-        // create JWTs
-        const accessToken = jwt.sign(
-            {
-                "UserInfo": {
-                    "username": foundUser.username,
-                    "roles": roles
-                }
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '10s' }
-        );
-        const refreshToken = jwt.sign(
-            { "username": foundUser.username },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: '1d' }
-        );
-        // Saving refreshToken with current user
-        foundUser.refreshToken = refreshToken;
-        const result = await foundUser.save();
-        console.log(result);
-        console.log(roles);
+      const roles = Object.values(foundUser.roles).filter(Boolean)
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            username: foundUser.username,
+            roles: roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '10s' },
+      )
 
-        // Creates Secure Cookie with refresh token
-        res.cookie('jwt', refreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+      const refreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '1d' },
+      )
+      // Saving refreshToken with current user
+      foundUser.refreshToken = refreshToken
+      const result = await foundUser.save()
 
-        // Send authorization roles and access token to user
-        res.json({ roles, accessToken });
+      // Creates Secure Cookie with refresh token
+      res.cookie('jwt', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      console.log('Access Token: ', accessToken)
+      console.log('Refresh Token: ', refreshToken)
 
+      // Send authorization roles and access token to user
+      res.status(200).json({ roles, accessToken })
     } else {
-        res.sendStatus(401);
+      res.sendStatus(401)
     }
+  }
 }
 
-module.exports = { handleLogin };
+module.exports = { handleLogin }
