cd9c09b7fa6c754591dc7eb25299e03f77036233
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-13 12:22:11 +0300

    allow all cors

--- server.js
+++ server.js
@@ -46,9 +46,9 @@ app.use('/', express.static(path.join(__dirname, '/public')))
 // routes
 app.use('/', require('./routes/root'))
 app.use('/register', require('./routes/register'))
-app.use('/auth', require('./routes/auth'))
-app.use('/refresh', require('./routes/refresh'))
-app.use('/logout', require('./routes/logout'))
+app.use('/auth', cors(corsOptions), require('./routes/auth'))
+app.use('/refresh', cors(corsOptions), require('./routes/refresh'))
+app.use('/logout', cors(corsOptions), require('./routes/logout'))
 
 app.use(verifyJWT)
 app.use('/employees', require('./routes/api/employees'))
