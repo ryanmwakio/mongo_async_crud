9ee3df55831f43f00332c81d28157d14a7769193
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-13 12:11:17 +0300

    allow all cors

--- server.js
+++ server.js
@@ -20,15 +20,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-//set all cors headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
-})
-
 // Connect to MongoDB
+
 connectDB()
 
 // custom middleware logger
