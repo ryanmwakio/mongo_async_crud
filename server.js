8292a25bd5c444d9f44b0a777c420929e77b52ca
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-13 10:23:15 +0300

    allow all cors

--- server.js
+++ server.js
@@ -13,17 +13,12 @@ const mongoose = require('mongoose')
 const connectDB = require('./config/dbConn')
 const PORT = process.env.PORT || 3500
 
+const corsOptions = {
+  origin: 'https://bitmama-rm.netlify.app',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 app.use(cors(corsOptions))
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  res.header('Access-Control-Allow-Credentials', true)
-  next()
-})
 
 // Connect to MongoDB
 connectDB()
