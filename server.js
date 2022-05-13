0a4fd87cbb0ed796daec3a03988e4bee63f2c40c
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-13 10:05:00 +0300

    cors

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ var corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://bitmama-rm.netlify.app') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
   )
+  res.header('Access-Control-Allow-Credentials', true)
   next()
 })
 
