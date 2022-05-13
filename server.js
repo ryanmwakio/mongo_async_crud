97464ed0e43d85406707aa71e13b0006a40964e6
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-13 12:38:39 +0300

    allow all cors

--- server.js
+++ server.js
@@ -13,8 +13,25 @@ const mongoose = require('mongoose')
 const connectDB = require('./config/dbConn')
 const PORT = process.env.PORT || 3500
 
+var allowedOrigins = [
+  'http://localhost:3000',
+  'https://bitmama-rm.netlify.app/login',
+  'https://bitmama-rm.netlify.app',
+]
+
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg =
+        'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  },
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
